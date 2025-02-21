1ff1223d6962c69f5029d569e6a1e1bc85c07259
mohittanwar94 <mo20570313@wipro.com>
2025-02-21 17:21:52 +0530

    remove unused code

--- corenetwork/src/main/java/com/example/corenetwork/network/ResultWrapper.kt
+++
@@ -1,6 +0,0 @@
-package com.example.corenetwork.network
-
-sealed class ResultWrapper<out T> {
-    data class Success<out T>(val data: T) : ResultWrapper<T>()
-    data class Failure<Nothing>(val msg: String) : ResultWrapper<Nothing>()
-}
